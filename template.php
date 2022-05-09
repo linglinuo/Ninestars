3d485b64d3da9f7d4073166d781c1bb93403d3c5
linglinuo <fiona051@gmail.com>
2022-05-09 13:43:08 +0800

    sort html to php v3
    vendorcss clear

--- template.php
+++ template.php
@@ -11,6 +11,18 @@ $top=
 <link href="img/favicon.png" rel="icon">
 <link href="img/apple-touch-icon.png" rel="apple-touch-icon">';
 
+$vendorcss=
+'<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+
+<!-- Vendor CSS Files -->
+<link href="vendor/aos/aos.css" rel="stylesheet">
+<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+<link href="vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+<link href="vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+<link href="vendor/swiper/swiper-bundle.min.css" rel="stylesheet">';
+
 $pageloader=
 '<!-- Page Loader -->
 <div id="loader-wrapper">

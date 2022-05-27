2edaa6b0068ce204e957a882aee612184cd2c709
supeihau <selina240@gmail.com>
2022-05-27 15:59:36 +0800

    forget pwd

---
+++ 忘記密碼.php
@@ -0,0 +1,144 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php
+    include ("template.php");
+    echo $top ;
+    echo $vendorcss;
+    echo $maincss;
+    echo $sourcejs;
+  ?>
+    <?php
+    //商品管理
+    include("mysql_connect.inc.php");
+
+    // 送出查詢的SQL指令
+    if ($result = mysqli_query($link, "SELECT * FROM products")) {
+    while ($row = mysqli_fetch_assoc($result)) {
+      $data .= "<tr><th scope=\"row\">$row[product_id]</th><td>$row[product_name]</td><td><img src=\"$row[product_picture]\" width=\"100px\"></td><td>$row[product_categories]</td><td>$row[product_type]</td><td>$row[product_price]</td></tr>";
+    }
+    mysqli_free_result($result); // 釋放佔用的記憶體
+    }
+    ?>
+
+
+  <style>
+    .error {
+      color: #D82424;
+      font-weight: normal;
+      font-family: "微軟正黑體";
+      display: inline;
+      padding: 1px;
+    }
+    .btn-new{
+      background-color: #eb5d1e;
+      color: #fef8f5;
+      border-radius: 10px;
+      padding-top: 3px;
+      padding-bottom: 3px;
+      padding-left: 10px;
+      padding-right: 10px;
+    }
+    .form-control{}
+  </style>
+</head>
+
+<body>
+  <?php
+    include ("template.php");
+    echo $pageloader ;
+  ?>
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">
+            <i class="fas fa-film mr-2"></i>
+            絲扇淒神社
+        </a>
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="index.php">首頁</a></li>
+          <li><a class="nav-link scrollto" href="index.php">介紹</a></li>
+          <li><a class="nav-link scrollto" href="store-page.php">小舖</a></li>
+          <li><a class="nav-link scrollto" href="求籤.php">求籤</a></li>
+          <li><a class="nav-link scrollto" href="解籤1.php">解籤</a></li>
+          <li><a class="nav-link scrollto" href="許願池.php">許願池</a></li>
+          <?php
+            if (isset($_SESSION['Name'])) {
+                if($_SESSION["Name"] == 'admin')
+                {
+                  echo '<li><a class="nav-link scrollto" href="商品管理.php">管理中心</a></li>';
+                  echo '<li><a href="logout.php">'.$_SESSION['Name'].'   登出</a></li>';
+                }
+                else
+                {
+                  echo '<li><a class="nav-link scrollto" href="訂單管理.php">訂單查詢</a></li>';
+                  echo '<li><a href="logout.php">'.$_SESSION['Name'].'   登出</a></li>';
+                }
+                } else {
+                echo '<li><button class="getstarted button1" onclick="document.getElementById(\'id01\').style.display=\'block\'" style="width: 125px; padding-top: 7px;">登入/註冊</button></li>';
+                }
+            ?>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+    </div>
+  </header><!--End Header -->
+  
+  <!--login-->
+  <?php
+  include ("login.php");
+  ?>
+  <!--login end-->
+  <main id="main" style="margin-top: 20px;">
+
+    <!-- ======= Breadcrumbs Section ======= -->
+    <section class="breadcrumbs">
+        <div class="container">
+          <div class="d-flex justify-content-between align-items-center">
+            <h2>忘記密碼</h2>
+          </div>
+        </div>
+    </section>
+    <!-- End Breadcrumbs Section -->
+
+    <section>
+        <div class="container" style="text-align: center; width: 500px">
+            <img src="img/foegetpwd.png">
+            <h5 class="mt-4">您的電子信箱</h5><br>
+            <input class="form-control" placeholder="輸入註冊時的電子信箱"><br>
+            <button class="btn btn-new">確認送出</button>
+        </div>
+    </section><!-- End Portfolio Section -->
+    
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <?php
+  include ("footer.html");
+  ?>
+  <!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <?php
+  include ("template.php");
+  echo $vendorjs ;
+  echo $mainjs ;
+  ?>
+  <!--verify-->
+  <?php
+    include ("verify.html");
+  ?>
+  <!--verify end-->
+</body>
+
+</html>
